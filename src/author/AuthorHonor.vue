332a05103662fdd5f4c4676b0c1e7f9a333a3ab9
Kaviilee <jiaweilee95@126.com>
2024-02-02 16:19:37 +0800

    perf: 更新称号组件

--- src/author/AuthorHonor.vue
+++ src/author/AuthorHonor.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 import JX3BOX from "@jx3box/jx3box-common/data/jx3box.json";
-import { getDecoration, getHonorJson } from "../../service/cms";
+import { getUserHonor } from "../../service/cms";
 import { inRange } from "lodash";
 
 const { __imgPath } = JX3BOX;
@@ -25,14 +25,19 @@ export default {
             },
         },
     },
+    computed: {
+        isJdt() {
+            return this.honor?.val?.toLowerCase()?.includes("jdt");
+        },
+    },
     methods: {
         imgUrl: function () {
             let item = this.honor;
             if (!item) return;
             if (item.isImgIndex) {
-                return __imgPath + `decoration/honor/${item.val}/${item.val}_${item.imgIndex}.${item.ext}`;
+                return __imgPath + `decoration/honor/${item.img}/${item.img}_${item.imgIndex}.${item.img_ext}`;
             }
-            return __imgPath + `decoration/honor/${item.val}/${item.val}.${item.ext}`;
+            return __imgPath + `decoration/honor/${item.img}/${item.img}.${item.img_ext}`;
         },
         getHonor() {
             let user_id = this.uid;
@@ -43,60 +48,68 @@ export default {
             try {
                 this.honor = JSON.parse(honor_local);
             } catch (err) {
-                getDecoration({ using: 1, user_id: user_id, type: "honor" }).then((data) => {
-                    let res = data.data.data || [];
-                    if (res.length == 0) {
+                getUserHonor(user_id).then((res) => {
+                    this.honor = res.data.data;
+                    if (!this.honor) {
                         //空 则为无主题，不再加载接口，界面设No
                         sessionStorage.setItem(HONOR_IMG_KEY + user_id, "no");
                         return;
                     }
-                    let honor = res[0];
-                    this.getHonorStyle(honor);
+                    this.getHonorStyle(this.honor);
                 });
             }
         },
         //有称号后，获取样式配置
-        getHonorStyle(data) {
-            getHonorJson().then((res) => {
-                let honorList = res.data;
-                let honorConfig = honorList[data.val];
-                let prefix = honorConfig.prefix;
-                let regPrefix = honorConfig.prefix.match(/\{([^{}]+?)\}/g);
-                let ranking = honorConfig.ranking;
-                let honorStr = honorConfig.year || "";
+        getHonorStyle(honor) {
+            const data = honor
+            let honorConfig = honor?.honor_info;
+            let only = honorConfig.only;
+            let prefix = honorConfig.prefix;
+            let regPrefix = honorConfig.prefix.match(/\{([^{}]+?)\}/g);
+            let ranking = honorConfig.ranking;
+            let honorStr = honorConfig.year || "";
+
+            console.log(honorConfig, regPrefix)
+            if (!only) {
                 if (regPrefix) {
                     honorStr = honorStr + (data[regPrefix[0].slice(1, -1)] || "");
                 } else {
                     honorStr = honorStr + prefix;
                 }
-                if (ranking.length > 0) {
-                    data.imgIndex = 0;
-                    for (let i = 0; i < ranking.length; i++) {
-                        if (data.ranking !== undefined && inRange(Number(data.ranking), ranking[i][0], ranking[i][1])) {
-                            data.imgIndex = i;
-                            let str = ranking[i][2];
-                            let regStr = str.match(/\{([^{}]+?)\}/g);
-                            if (regStr) {
-                                honorStr =
-                                    honorStr +
-                                    str.replace(/\{([^{}]+?)\}/g, function (match, p1) {
-                                        return data[p1] || "";
-                                    });
-                            } else {
-                                honorStr = honorStr + str;
-                            }
-                            break;
+            } else {
+                honorStr = prefix;
+            }
+            if (ranking.length > 0) {
+                data.imgIndex = 0;
+                for (let i = 0; i < ranking.length; i++) {
+                    if (
+                        honor.ranking !== undefined &&
+                        inRange(Number(honor.ranking), ranking[i][0], ranking[i][1])
+                    ) {
+                        data.imgIndex = i;
+                        let str = ranking[i][2];
+                        let regStr = str.match(/\{([^{}]+?)\}/g);
+                        if (regStr) {
+                            honorStr =
+                                honorStr +
+                                str.replace(/\{([^{}]+?)\}/g, function (match, p1) {
+                                    return data[p1] || "";
+                                });
+                        } else {
+                            honorStr = honorStr + str;
                         }
+                        break;
                     }
                 }
-                data.honor = honorStr + honorConfig.suffix;
-                data.color = honorConfig.color;
-                data.ext = honorConfig.ext;
-                data.isHave = true;
-                data.isImgIndex = ranking.length > 0;
-                sessionStorage.setItem(HONOR_IMG_KEY + this.uid, JSON.stringify(data));
-                this.honor = data;
-            });
+            }
+            data.honor = honorStr + honorConfig.suffix;
+            data.color = honorConfig.color;
+            data.img = honorConfig.img;
+            data.img_ext = honorConfig.img_ext;
+            data.isHave = true;
+            data.isImgIndex = ranking.length > 0;
+            sessionStorage.setItem(HONOR_IMG_KEY + this.uid, JSON.stringify(data));
+            this.honor = data;
         },
     },
 };
